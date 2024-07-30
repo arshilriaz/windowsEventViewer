714fed16f4fc87e757a5ddf9bf82c6283e69169c
Rohith0007 <50866037+Rohith0007@users.noreply.github.com>
2024-07-30 11:30:06 -0400

    ][ Handling, Preprocess define, Commands modification, Append and Extend handled fro new data, Defined Info button

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import subprocess
 import time
 from flask import Response
-from app.service import host_verification, fetch_logs
+from app.service import host_verification, fetch_logs, preprocess
 import platform
 load_dotenv()
 
@@ -58,9 +58,6 @@ def upload_evtx():
         username=host_json_data["details"][0]['username']
         password=host_json_data["details"][0]['password']
  
-        file_name = f"{hostname}_{username}_{password}.json"
-        file_path = os.path.join(os.getenv("MODEL_OUTPUT_DIR"), file_name).replace("\\", "/")
- 
         evtx_file_name = f"{hostname}_{username}_{password}.evtx"
         evtx_file_path = os.path.join(os.getcwd() + "/" + os.getenv("MODEL_INPUT_EVTX"), evtx_file_name).replace("\\", "/")
         evtxFile.save(evtx_file_path)    
@@ -76,15 +73,28 @@ def upload_evtx():
                 "sh", "-c", f"cd python-evtx/scripts && python evtx_dump.py {evtx_file_path} {hostname} {username} {password}"
             ]
         subprocess.run(command)
+
+        # Sometimes files are having ][ so just replacing that with ','
+        file_name = f"{hostname}_{username}_{password}.json"
+        file_path = os.path.join(os.getenv("MODEL_OUTPUT_DIR"), file_name)
+
+        with open(file_path, 'r') as file:
+            file_contents = file.read()
+            file_contents = file_contents.replace('][', ',')
+
+        # Write the modified contents back to the file
+        with open(file_path, 'w') as file:
+            file.write(file_contents)
+
         if new_unique_data:
+            preprocess()
             existing_data["details"].extend(host_json_data["details"])
  
             with open(os.getenv("MODEL_INPUT_DIR").replace("\\", "/"), 'w') as f:
                 json.dump(existing_data, f, indent=4)
  
         with open(os.getenv("MODEL_INPUT_DIR").replace("\\", "/"), 'r') as f:
-            hosts = json.load(f)
-            return render_template('hosts.html', hosts=hosts)
+            return redirect(url_for('main.hosts'))
        
     except Exception as e:
         print(e)
@@ -133,8 +143,35 @@ def stream():
             # Fetch new logs and update the last fetched timestamp
             new_logs, last_fetched = fetch_logs(hostname, username, password, last_fetched)
             if new_logs:
-                all_logs.extend(new_logs) 
+                if not all_logs:
+                    all_logs.extend(new_logs)
+                else:
+                    all_logs.append(new_logs) 
                 yield f"data: {json.dumps(all_logs, indent=4, sort_keys=True)}\n\n"
             time.sleep(15)
 
     return Response(generate(), mimetype="text/event-stream")
+
+@main.route('/info', methods=['POST'])
+def info():
+    hostname = request.form['hostname']
+    username = request.form['username']
+    password = request.form['password']
+
+    file_name = f"{hostname}_{username}_{password}.json"
+    file_path = os.path.join(os.getenv("MODEL_OUTPUT_DIR").replace("\\", "/"), file_name)
+
+    # with open(file_path, "r") as f:
+    #     data = json.load(f)
+    #     hostname = data['hostname']
+    #     username = data['username']
+    #     password = data['password']
+
+    #     total_events = data['application'][0]['Index']
+    #     unique_events = set()
+
+    #     for d in data['application']:
+    #         unique_events.add(d['EventID'])
+
+
+    return render_template('info.html')
